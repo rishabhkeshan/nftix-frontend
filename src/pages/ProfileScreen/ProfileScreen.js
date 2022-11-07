3344808c32cbd5906343c8977b60a2f5d8130332
Hemanth Krishna <hkpdev008@gmail.com>
2022-11-07 17:53:30 +0530

    feat: profile integration

--- src/pages/ProfileScreen/ProfileScreen.js
+++ src/pages/ProfileScreen/ProfileScreen.js
@@ -1,36 +1,36 @@
 import "./ProfileScreen.scss";
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import AssetMantleFunctions from "../../blockchain/assetmantle";
 
 export default function ProfileScreen() {
-  const fetched = {
-    data: {
-      id: "6368b298fbd38ea3f8b23444",
-      username: "benro",
-      email: "hey@benro.dev",
-      hash_pwd: "$2a$10$QgddOYP4hDanG0lTrsJfbOH5N5PGE3SpeZKpx4D.x8r9MGfl/yNYG",
-      wallet_id: "mantle17acqqlnrc62v6qegp38rptukg7fhmr0d02etl7",
-      nub_id:
-        "devnet-mantle-1.cGn3HMW8M3t5gMDv-wXa9sseHnA=|qn00KUqTBnWFAkg0B5LIQYHnE88=",
-      hashed_nem:
-        "U2FsdGVkX18c6/INKUl8bx2WNqP5vANeBuIGcoTREeLrLdNWeqIsI56Pqq5Ri7Pb3EQSafytHnVYnw5vAS4zE+W2nKeI2BDyfUTo1TvgGApApv8ffQNkuXIxZuBio2IwP3o/lPEJUt0r559PTfKluL2oVGhm+7vXcwQyJWwsAxKYKgss9OMmbCCYbdtK9vjVOpyijU2TNMDDCRv/Qlk2Ci1I2ozZk82TYeAMvtZqm6yaowzhblyK7OIbEP6DaIdhneWpGIfXN54G65ijHUc6BA==",
-      is_org: true,
-      is_new: false,
-    },
-    description: "user data fetched!",
-    status: true,
-  };
-  const userData = fetched.data;
-  // I did this for simplicity
+  const [userData, setUserData] = useState({});
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const data = JSON.parse(localStorage.getItem("deto"));
+      setUserData(data);
+      const mantleFunctions = new AssetMantleFunctions(data.username);
+      mantleFunctions.walletId = data.wallet_id;
+      const bal = await mantleFunctions.getAccountBalance();
+      setBalance(bal);
+    })();
+  }, []);
+
   return (
     <div className="profilescreen">
       <div className="profilescreen_maincontainer">
         <div>
           <div className="profilescreen_maincontainer_title">Username</div>
-          <div className="profilescreen_maincontainer_subtitle">{userData?.username}</div>
+          <div className="profilescreen_maincontainer_subtitle">
+            {userData?.username}
+          </div>
         </div>
         <div>
           <div className="profilescreen_maincontainer_title">Email</div>
-          <div className="profilescreen_maincontainer_subtitle">{userData?.email}</div>
+          <div className="profilescreen_maincontainer_subtitle">
+            {userData?.email}
+          </div>
         </div>
         <div>
           <div className="profilescreen_maincontainer_title">Identity ID</div>
@@ -44,7 +44,9 @@ export default function ProfileScreen() {
           {userData?.wallet_id}
         </div>
         <div className="profilescreen_maincontainer_title">Balance</div>
-        <div className="profilescreen_maincontainer_subtitle">995 MNTL</div>
+        <div className="profilescreen_maincontainer_subtitle">
+          {balance === 0 ? "Loading" : balance} MNTL
+        </div>
       </div>
     </div>
   );
